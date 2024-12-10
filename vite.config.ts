5d9faa3219fa97cb61a75221e067b9e36121185d
ngrvsn <forpomoika2@gmail.com>
2024-12-11 01:04:11 +0300

    фикс

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
- plugins: [react()],
- resolve: {
-   alias: {
-     '@components': path.resolve(__dirname, './src/components'),
-     '@assets': path.resolve(__dirname, './src/assets'),
-     '@hooks': path.resolve(__dirname, './src/hooks'),
-     '@shared': path.resolve(__dirname, './src/shared')
-   }
- }
-});
+  base: '/royal/',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@shared': path.resolve(__dirname, './src/shared')
+    }
+  }
+})
