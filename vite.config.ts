36860d98e385b689f7d9d9d0e50c4cdde0abad31
ngrvsn <forpomoika2@gmail.com>
2024-12-12 05:33:23 +0300

    доработки

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,8 @@ export default defineConfig({
       '@components': path.resolve(__dirname, './src/components'),
       '@assets': path.resolve(__dirname, './src/assets'),
       '@hooks': path.resolve(__dirname, './src/hooks'),
-      '@shared': path.resolve(__dirname, './src/shared')
+      '@shared': path.resolve(__dirname, './src/shared'),
+      '@services': path.resolve(__dirname, './src/services')
     }
   },
   build: {
